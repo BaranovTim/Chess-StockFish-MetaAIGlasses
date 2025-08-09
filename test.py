94224283bf44f4c742d0ac67c4b3aa34b6012ad9
Sjx0ck <timofejtakoj@gmail.com>
2025-08-08 22:47:02 -0700

    test

---
+++ test.py
@@ -0,0 +1,28 @@
+import cv2 as cv
+
+camera = cv.VideoCapture(0, cv.CAP_DSHOW)
+
+# Проверяем, открыта ли камера
+if not camera.isOpened():
+    print("Не удалось открыть камеру")
+    exit()
+
+# Бесконечный цикл захвата кадров
+while True:
+    # Считываем кадр
+    ret, frame = camera.read()
+
+    if not ret:
+        print("Не удалось считать кадр")
+        break
+
+    # Отображаем кадр
+    cv.imshow('Видеопоток с камеры', frame)
+
+    # Выход из цикла по нажатию 'q'
+    if cv.waitKey(1) & 0xFF == ord('q'):
+        break
+
+# Освобождаем ресурсы
+camera.release()
+cv.destroyAllWindows()
